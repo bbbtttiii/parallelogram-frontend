b2381b9b0c645df63e6c08f1eb0ba7eee27d1dae
bbbtttiii <ben.iburg@gmail.com>
2020-11-20 15:09:40 -0600

    add tag to articles

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_204507) do
+ActiveRecord::Schema.define(version: 2020_11_20_210354) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "tag"
   end
 
 end
