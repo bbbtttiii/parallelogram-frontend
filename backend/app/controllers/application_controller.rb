ca09e99514437e3ec7e8feb557ba9d7f0c1e4398
bbbtttiii <ben.iburg@gmail.com>
2020-11-22 11:36:26 -0600

    comment in cors initializer

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
-    helpers do
-        def current_user
-            User.first
-        end
-            
-        def logged_in?
-            !!current_user
-        end
+
+    def current_user
+        User.first
     end
+        
+    def logged_in?
+        !!current_user
+    end
+
 end
