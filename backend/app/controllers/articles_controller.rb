a24820e01f6dc927a8441fa5736208eb5a0ddf11
bbbtttiii <ben.iburg@gmail.com>
2020-11-22 09:58:23 -0600

    rebuild users with scaffold

--- backend/app/controllers/articles_controller.rb
+++ backend/app/controllers/articles_controller.rb
@@ -14,29 +14,29 @@ class ArticlesController < ApplicationController
   end
 
   # POST /articles
-  def create
-    @article = Article.new(article_params)
+  # def create
+  #   @article = Article.new(article_params)
 
-    if @article.save
-      render json: @article, status: :created, location: @article
-    else
-      render json: @article.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @article.save
+  #     render json: @article, status: :created, location: @article
+  #   else
+  #     render json: @article.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /articles/1
-  def update
-    if @article.update(article_params)
-      render json: @article
-    else
-      render json: @article.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @article.update(article_params)
+  #     render json: @article
+  #   else
+  #     render json: @article.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /articles/1
-  def destroy
-    @article.destroy
-  end
+  # def destroy
+  #   @article.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
