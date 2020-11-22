81b9dfd2b65bb2d927ab025e5b222acf4c3c4d03
bbbtttiii <ben.iburg@gmail.com>
2020-11-22 09:31:22 -0600

    add users model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_152642) do
+ActiveRecord::Schema.define(version: 2020_11_22_152940) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_11_22_152642) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
   end
 
 end
