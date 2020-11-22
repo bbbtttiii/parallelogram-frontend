07fb37cb3b8b1a7ea3d670b1e3ab3a74c199baef
bbbtttiii <ben.iburg@gmail.com>
2020-11-22 16:06:08 -0600

    add react bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import store from './store.js'
-
+// named vs default export
 
 ReactDOM.render(
   <Provider store={store}>
