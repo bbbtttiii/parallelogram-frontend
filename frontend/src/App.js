40ddae59f8a130a1d584ca633fd3351a539977f2
bbbtttiii <ben.iburg@gmail.com>
2020-11-23 21:27:22 -0600

    adds react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './index.css';
 import { connect } from 'react-redux';
-import { getCurrentUser } from './actions/currentUser.js'
-import Nav from './components/Nav.js'
-import Main from './components/Main.js'
+import { getCurrentUser } from './actions/currentUser.js';
+import Nav from './components/Nav.js';
+import Main from './components/Main.js';
+import Login from './components/Login.js';
+import Logout from './components/Logout.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends React.Component {
 
@@ -13,10 +16,15 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Nav />
-        <Main />
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Main />
+
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/logout' component={Logout} />
+        </div>
+      </Router>
     );
   }
 }
