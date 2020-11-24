c334b6783956a51189ba4d92581084e6168e8bfa
bbbtttiii <ben.iburg@gmail.com>
2020-11-24 10:09:12 -0600

    add user validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :favorites
     has_many :articles, through: :favorites
+    validates :username, :email, uniqueness: true, presence: true
+    validates :password_digest, presence: true, length: {:within => 6..40},
 end
