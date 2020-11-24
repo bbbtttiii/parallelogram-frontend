ede32790735df54b081eedd7e2c74a8a190dd269
bbbtttiii <ben.iburg@gmail.com>
2020-11-24 16:36:20 -0600

    added Grid component

---
+++ frontend/src/components/Grid.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from './Card.js';
+
+const Grid = () => {
+  return (
+    <div className="Grid">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+    </div>
+  )
+}
+
+export default Grid;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import Card from './Card.js';
+import Grid from './Grid';
 
 const Main = () => {
   return (
     <div className="Main">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Grid />
     </div>
   )
 }
