9f78133bcd1743c8bd8e89629d60413abf9d3bec
bbbtttiii <ben.iburg@gmail.com>
2020-11-24 10:11:44 -0600

    add initialState to signup

--- frontend/src/reducers/signupForm.js
+++ frontend/src/reducers/signupForm.js
@@ -1,6 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 
-export default (state, action) => {
+const initialState = {
+  username: '',
+  email: '',
+  password: ''
+}
+
+export default (state=initialState, action) => {
   switch (action.type) {
     default:
       return state
