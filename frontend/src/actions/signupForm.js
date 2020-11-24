2d422ddba1a19531c58bbb31ddb573fed4d4b2f0
bbbtttiii <ben.iburg@gmail.com>
2020-11-24 10:13:20 -0600

    add signup action creators

--- frontend/src/actions/signupForm.js
+++ frontend/src/actions/signupForm.js
@@ -1,8 +1,12 @@
-// sync
+export const updateSignupForm = (formData) => {
+  return {
+    type: 'UPDATE_SIGNUP_FORM',
+    formData
+  }
+}
 
-
-
-
-
-// async
-
+export const resetSignupForm = () => {
+  return {
+    type: "RESET_SIGNUP_FORM"
+  }
+}
