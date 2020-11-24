616681433425d696239c108611aa5e74ec98500c
bbbtttiii <ben.iburg@gmail.com>
2020-11-24 14:23:53 -0600

    change buttons to links

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux';
 import Login from './Login.js';
 import Logout from './Logout.js';
 import Signup from './Signup.js';
+import { Link } from 'react-router-dom';
 
-const Nav = ({ currentUser }) => {
+const Nav = ({ currentUser, loggedIn }) => {
   return (
     <div className="Nav">
       <span className="left-nav">
@@ -14,12 +15,13 @@ const Nav = ({ currentUser }) => {
       <span className="right-nav">
         {/* if logged out, show login/signup buttons*/}
         {/* {if logged in, show favorites/logout buttons} */}
-        {currentUser ? <> <Pinned/> <Logout /> </> : <> <Login /> <Signup /> </> }
+        {/* {loggedIn ? <> <Logout /> </> : <> <Login /> <Signup /> </> } */}
         {/* <Route exact path='/' render{() => loggedIn ? <LOGGEDINCOMPONENTS/> : <NOTLOGGEDINCOMPONENTS/>} /> */}
+        
+        {/* {currentUser ? <strong>Hi {currentUser.attributes.username}</strong> : ""} */}
+        <Link to='/login'>Log In</Link> or <Link to ='/signup'>Sign Up</Link>
+        {/* {currentUser ? <Logout /> : <Login />} */}
       </span>
-      {/* {currentUser ? <strong>Hi {currentUser.attributes.username}</strong> : ""} */}
-      <button>Log In</button> or <button>Sign Up</button>
-      {/* {currentUser ? <Logout /> : <Login />} */}
     </div>
   )
 }
