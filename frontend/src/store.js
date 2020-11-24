721955e61ffc3e272b7d8d0997b2e58d7abfa504
bbbtttiii <ben.iburg@gmail.com>
2020-11-24 10:03:17 -0600

    add signup reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,13 +3,15 @@ import usersReducer from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm.js'
 import favorites from './reducers/favorites.js'
+import signupForm from './reducers/signupForm.js'
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
   user: usersReducer,
   currentUser,
   loginForm,
-  favorites
+  favorites,
+  signupForm
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
