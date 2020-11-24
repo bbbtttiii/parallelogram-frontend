621f5df9dd26d115782d48ccb88130e0fc0a290c
bbbtttiii <ben.iburg@gmail.com>
2020-11-24 16:06:12 -0600

    changes to card spacing

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,8 +5,10 @@ const Card = () => {
     <div className="Card">
       <h1 className="card-title">This is a card</h1>
       <div className="card-photo">Photo</div>
-      <div className="card-details">By Asdf Asdf - Science</div>
-      <div className="card-blurb">ASsfd sdf lsdf sdf sdfsfd sdf sdf sdf sdf sdf sdf sdf </div>
+      <div className="card-details">By Asdf Asdf <a href="#">Science</a></div>
+      <div className="card-blurb">
+        ASsfd sdf lsdf sdf sdfsfd sdf sdf sdf sdf sdf fsdsdf sdf. sdglkjsdf  sadf sdflskdjfsdfsdf sf sdfsdf sdf s sdf sdflsk.
+      </div>
     </div>
   )
 }
