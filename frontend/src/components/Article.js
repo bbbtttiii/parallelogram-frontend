fca8d7ced40f116398bc968fa60c962bf40b7262
bbbtttiii <ben.iburg@gmail.com>
2020-11-25 12:31:18 -0600

    set up article page wireframe

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Card = () => {
   return (
     <div className="Article">
-      Article asdfasdfasdf.
+      <div className="article-title">Title</div>
+      <div className="article-photo">Image</div>
+      <div className="article-details">Details</div>
+      <div className="article-content">Contenttttttttt</div>
     </div>
   )
 }
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Card = () => {
   return (
     <div className="Card">
-      <h1 className="card-title"><a href="#">This is a card title. What happens when it gets really long huh?</a></h1>
+      <h1 className="card-title"><NavLink to='/article'>This is a card title. What happens when it gets really long huh?</NavLink></h1>
       <div className="card-photo">Photo</div>
-      <div className="card-details">By Asdf Asdf <a href="#">Science</a></div>
-      <div className="card-blurb">
+      <div className="card-details">By Asdf Asdf <NavLink to='/tag'>Science</NavLink></div>
+      <div className="card-summary">
         ASsfd sdf lsdf sdf sdfsfd sdf sdf sdf sdf sdf fsdsdf sdf. sdglkjsdf  sadf sdflskdjfsdfsdf sf sdfsdf sdf s sdf sdflsk.
       </div>
     </div>
@@ -14,3 +15,9 @@ const Card = () => {
 }
 
 export default Card;
+
+
+// For dynamic Links:
+
+// let articleId = {article.id}
+// <NavLink to={`/${articleId}`} className="nav-link">TEXT</NavLink>
