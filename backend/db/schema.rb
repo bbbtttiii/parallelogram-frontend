4ec3127e8ba5485b77dc34dc45f054697ed1217e
bbbtttiii <ben.iburg@gmail.com>
2020-11-25 11:23:45 -0600

    add article summary

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_163430) do
+ActiveRecord::Schema.define(version: 2020_11_25_172240) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.string "tag"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
     t.integer "favorite_count"
+    t.string "summary"
   end
 
   create_table "favorites", force: :cascade do |t|
