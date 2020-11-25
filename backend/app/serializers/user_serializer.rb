43a96a498dca8371abdcfa8e68d7cfd45187119d
bbbtttiii <ben.iburg@gmail.com>
2020-11-25 10:32:49 -0600

    remove other serializers for now

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :email
-  has_many :favorites, serializer: FavoritesSerializer
+  has_many :favorites
   has_many :articles, through: :favorites
 end
