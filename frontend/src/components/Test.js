d06fceb8dcd7eac12a75ba89dfa339d0b2cfa14b
bbbtttiii <ben.iburg@gmail.com>
2020-11-28 17:05:19 -0600

    embed card photos, fix scaling

--- frontend/src/components/Test.js
+++ frontend/src/components/Test.js
@@ -5,7 +5,7 @@ import Card from './Card.js';
 
 const Test = props => {
   console.log("in test component", props.articles)
-  const articleCards = props.articles.map(a => <Card key={a.id} title={a.attributes.title} author={a.attributes.author} summary={a.attributes.summary} tag={a.attributes.tag} />) 
+  const articleCards = props.articles.map(a => <Card key={a.id} title={a.attributes.title} author={a.attributes.author} summary={a.attributes.summary} tag={a.attributes.tag} image_url={a.attributes.image_url}/>) 
   // const articleCards = props.articles.map(a => (<p key={a.id}><NavLink to={`/${a.id}`}>{a.attributes.title}</NavLink></p>))
   return articleCards
 }
