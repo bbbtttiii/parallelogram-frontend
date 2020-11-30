dc67627cb0168c25134b222d40a485e501698372
bbbtttiii <ben.iburg@gmail.com>
2020-11-30 16:49:28 -0600

    revert to less dry logic

--- frontend/src/components/ArticleContent.js
+++ frontend/src/components/ArticleContent.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Aside from './Aside.js'
-// import { render } from 'react-dom';
-// import { NavLink } from 'react-router-dom';
-
 
 //make functional?
 class ArticleContent extends React.Component {
 
   render() {
 
-    let articleMatch = this.props.articles.find(a => a.id === this.props.id).attributes
-    
     const findArticle = () => {
-      return articleMatch
+      return this.props.articles.find(a => a.id === this.props.id).attributes
     }
 
     return (
       <div className="Article">
         <div className="article-title">
-          {articleMatch && findArticle().title}
+          {this.props.articles.find(a => a.id === this.props.id) && findArticle().title}
         </div>
         <div className="article-details">
-          {articleMatch && findArticle().author}
+          {this.props.articles.find(a => a.id === this.props.id) && findArticle().author}
         </div>
         <div className="article-photo">
-          <img src={`${articleMatch && findArticle().image_url}`} alt="Parallelogram" />
+          <img src={`${this.props.articles.find(a => a.id === this.props.id) && findArticle().image_url}`} alt="Parallelogram" />
         </div>
         <div className="article-content">
-          {articleMatch && findArticle().content}
+          {this.props.articles.find(a => a.id === this.props.id) && findArticle().content}
         </div>
       </div>
     )
   }
 }
 
-const mapStateToProps = (state, { currentUser }) => {
+const mapStateToProps = state => {
   return {
-    articles: state.article.articles,
-    currentUser,
-    loggedIn: !!currentUser
+    articles: state.article.articles
   }
 }
 
