91677af6893e272b3d0e2f234e8a9572e22a5a65
bbbtttiii <ben.iburg@gmail.com>
2020-11-29 18:45:28 -0600

    convert ArticleContent to class component

--- frontend/src/components/ArticleContent.js
+++ frontend/src/components/ArticleContent.js
@@ -1,42 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+// import { render } from 'react-dom';
 import { connect } from 'react-redux';
 
-const ArticleContent = props => {
-  
-  //HOOK:
-  // initial state
-  // useState - hook (like setState but for functional components)
-  // setting variable article, and setting a way to change article
-  const [article, setArticle] = useState({})
-  console.log(article)
+class ArticleContent extends React.Component {
 
-  // like componentDidMount - after render, will run everything inside useEffect
-  // setArticle 
-  // 17: dependencies - will execute whenever those things change
-  useEffect(() => {
-    setArticle(props.articles.find(a => a.id === props.id).attributes)
-  }, [props.articles, props.id])
+  constructor() {
+    super()
+    this.state = []
+  }
 
-  // const findArticle = () => {
-  //   return props.articles.find(a => a.id === props.id)
-  // }
-  
-  return (
-    <div className="Article">
-      <div className="article-title">
-        {article && article.title}
+  render() {
+
+  const findArticle = () => {
+      return this.props.articles.find(a => a.id === this.props.id).attributes
+  }
+
+    return (
+      <div className="Article">
+        <div className="article-title">
+          {findArticle().title}
+        </div>
+        <div className="article-photo">
+          <img src={`${findArticle().image_url}`} alt="Parallelogram" />
+        </div>
+        <div className="article-details">
+          {findArticle().author} - {findArticle().tag}
+        </div>
+        <div className="article-content">
+          {findArticle().content}
+        </div>
       </div>
-      <div className="article-photo">
-        <img src={`${article && article.image_url}`} alt="Parallelogram" />
-      </div>
-      <div className="article-details">
-        {article && article.author} — {article && article.tag}
-      </div>
-      <div className="article-content">
-        {article && article.content}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = state => {
