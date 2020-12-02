4b83a166f3d2cfeef903f927a1262e5f3288ea98
bbbtttiii <ben.iburg@gmail.com>
2020-12-02 06:34:48 -0600

    remove mapStateToProps from Article

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ArticleContent from './ArticleContent.js'
-import { connect } from 'react-redux'
 import Sidebar from './Sidebar.js'
 
 const Article = props => {
   return (
     <>
-        <Sidebar articleId={props.match.params.id}/>
-        <ArticleContent id={props.match.params.id}/>
+      <Sidebar articleId={props.match.params.id}/>
+      <ArticleContent id={props.match.params.id}/>
     </>
   )
 }
 
-const mapStateToProps = state => {
-  return {
-    articles: state.articles.articles,
-  }
-}
-
-export default connect(mapStateToProps)(Article);
+export default Article;
