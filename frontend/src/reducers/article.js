2d7783b0e5e62f854e6ee2d9309b4943fde197b1
bbbtttiii <ben.iburg@gmail.com>
2020-12-09 13:36:01 -0600

    article reducers use loose equality

--- frontend/src/reducers/article.js
+++ frontend/src/reducers/article.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 /* eslint-disable import/no-anonymous-default-export */
 
 const initialState = {
@@ -9,16 +10,12 @@ export default (state = initialState, action) => {
     case 'ADD_ARTICLES':
       return { ...state, articles: action.articles }
     case "ADD_FAVORITE":
-
       //find current article
-      let currentArticle = state.articles.find(a => a.id === action.favorite.data.attributes.article_id)
-
+      let currentArticle = state.articles.find(a => a.id == action.favorite.data.attributes.article_id)
       //added to list
       currentArticle.relationships.favorites.data = [...currentArticle.relationships.favorites.data, action.favorite]
-
       //map through collection of articles and replace with updated version
-      let articles = state.articles.map(a => (a.id === currentArticle.id ? currentArticle : a))
-
+      let articles = state.articles.map(a => (a.id == currentArticle.id ? currentArticle : a))
       return { articles }
     default:
       return state;
