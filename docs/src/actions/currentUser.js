6cf3a123b577e551ff34eb75fa2b3cff692df69a
bbbtttiii <ben.iburg@gmail.com>
2020-12-11 13:44:16 -0600

    recreate docs

---
+++ docs/src/actions/currentUser.js
@@ -0,0 +1,99 @@
+import { resetLoginForm } from './loginForm.js'
+import { resetSignupForm } from './signupForm.js'
+
+// synchronous action creators - no data to fetch - pure functions
+export const setCurrentUser = user => {
+  return {
+    type: 'SET_CURRENT_USER',
+    user
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: 'CLEAR_CURRENT_USER'
+  }
+}
+
+// asynchronous action creators (redux-thunk)
+export const login = (info, history) => {
+  return dispatch => {
+    return fetch('http://localhost:3001/api/v1/login', {
+      credentials: "include",
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(info)
+    })
+    .then(r => r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        dispatch(setCurrentUser(response.data))
+        dispatch(resetLoginForm())
+        history.push('/')
+      }
+    })
+    .catch(console.log)
+  }
+}
+
+export const signup = (info, history) => {
+  return dispatch => {
+    const userInfo = {
+      user: info
+    }
+    return fetch('http://localhost:3001/api/v1/signup', {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    })
+    .then(r => r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        dispatch(setCurrentUser(response.data))
+        dispatch(resetSignupForm())
+        history.push('/')
+      }
+    })
+    .catch(console.log)
+  }
+}
+
+export const logout = () => {
+  return (dispatch) => {
+    dispatch(clearCurrentUser())
+    return fetch('https://parallelogram-api.herokuapp.com/api/v1/logout', {
+      credentials: 'include',
+      method: 'DELETE'
+    })
+  }
+}
+
+export const getCurrentUser = () => {
+  return dispatch => {
+    return fetch('https://parallelogram-api.herokuapp.com/api/v1/get_current_user', {
+      credentials: "include",
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(r => r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        dispatch(setCurrentUser(response.data))
+      }
+    })
+    .catch(console.log)
+  }
+}
---
+++ docs/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import usersReducer from './reducers/users.js'
+import currentUser from './reducers/currentUser.js'
+import loginForm from './reducers/loginForm.js'
+import signupForm from './reducers/signupForm.js'
+import article from './reducers/article.js'
+import thunk from 'redux-thunk'
+
+const reducer = combineReducers({
+  user: usersReducer,
+  currentUser: currentUser,
+  loginForm: loginForm,
+  signupForm: signupForm,
+  articles: article
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
