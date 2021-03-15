2e859a4417dc668992e1885130993626b2ad499d
bbbtttiii <ben.iburg@gmail.com>
2021-03-15 17:42:51 -0500

    update loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,10 +3,13 @@ import React from 'react'
 const Loading = () => {
 
   return(
-    <div class="loading-spinner">
-      <div class="loading">
-        <div></div>
+    <div class="loading-container">
+      <div class="loading-spinner">
+        <div class="loading">
+          <div></div>
+        </div>
       </div>
+      <div><p>Loading articles...</p></div>
     </div>
   )
 }
